65ab455cf86c4d2ed6dd2038513e7f06a2c3585c
rimasultana <rimasultana20225@gmail.com>
2024-09-16 01:58:44 +0600

    onclick

--- Module-25/event.js
+++ Module-25/event.js
@@ -14,8 +14,28 @@ function makeBlue(){
     document.body.style.backgroundColor = 'blue'
 }
 
-// option-3 another
+// option-4
 const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple(){
    document.body.style.backgroundColor = 'purple';
 }
+
+// option - 5
+const pinkButton =document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+function makePink(){
+  document.body.style.backgroundColor = 'pink'
+}
+
+// option- 6
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+  document.body.style.backgroundColor = 'green';
+})
+
+// mostly use korbo option-4
+// important
+
+document.getElementById('make-goldenrod') .addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
