ec5fb1b079559962bb447e6b03ce34227c8b827a
rimasultana <rimasultana20225@gmail.com>
2024-09-20 21:10:18 +0600

    third-step

--- Payoo-MFS/js/home.js
+++ Payoo-MFS/js/home.js
@@ -1,8 +1,25 @@
 // add money to the account
-document.getElementById('btn-addMoney').addEventListener('click', function(event){
+document
+  .getElementById("btn-addMoney")
+  .addEventListener("click", function (event) {
     // prevent page reload after form submit
     event.preventDefault();
-    const addInputMoney = document.getElementById('input-add-money').value;
+    const addInputMoney = document.getElementById("input-add-money").value;
 
-    const pinInputNumber = document.getElementById('input-pin-number').value;
-})
+    const pinInputNumber = document.getElementById("input-pin-number").value;
+
+    if (pinInputNumber === "1234") {
+      console.log("adding money to your account");
+      const balance = document.getElementById('balance').innerText;
+
+      const addNumberInputMoney = parseFloat(addInputMoney);
+      const addNumberbalanceMoney = parseFloat(balance);
+
+    const newMoney =addNumberInputMoney + addNumberbalanceMoney;
+       document.getElementById('balance').innerText = newMoney;
+
+    }
+    else{
+        alert("Faild to add money! please try again")
+    }
+  });
