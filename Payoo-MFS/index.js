cd2bfded19f54838e2bce311ccf9ea4dc2d55d6b
rimasultana <rimasultana20225@gmail.com>
2024-09-20 20:48:01 +0600

    second-step

--- Payoo-MFS/index.js
+++ Payoo-MFS/index.js
@@ -1,13 +1,37 @@
 // from submit reloadin page
 
 // Step-1
-document
+/* document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
     //   step-2
     event.preventDefault();
     // step-3
-    const phoneNumber = document.getElementById('phone-number').value;
+    const phoneNumber = document.getElementById("phone-number").value;
 
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+     // step-4
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      console.log("You are logged in");
+    }
+    // step
+    else{
+      console.log("Wrong phone or pin number");
+    }
+  }); */
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    // console.log(phoneNumber, pinNumber);
+    // bad way to validate
+
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      console.log("you are login");
+      window.location.href = 'home.html';
+    }else{
+      alert('wromg phone number or pin');
+    }
   });
---
+++ Payoo-MFS/js/home.js
@@ -0,0 +1,8 @@
+// add money to the account
+document.getElementById('btn-addMoney').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+    const addInputMoney = document.getElementById('input-add-money').value;
+
+    const pinInputNumber = document.getElementById('input-pin-number').value;
+})
